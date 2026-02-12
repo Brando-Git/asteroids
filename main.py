9c42d8f18c9849a67bcf787259634116b7e3c5d7
Brando-Git <bcharri3@gmail.com>
2026-02-11 21:18:12 -0600

    D: Added Player to screen (not controlled)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -9,13 +10,27 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    gameclock = pygame.time.Clock()
+    dt = 0
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
 
     while(True):
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+
+        #player rendering
+        player.draw(screen)
+
         pygame.display.flip()
+
+        # limit framerate to 60 FPS
+        dt = (gameclock.tick(60) / 1000)
+
 if __name__ == "__main__":
     main()
